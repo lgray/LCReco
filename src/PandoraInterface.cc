712ecf8a5989e0b5bd919972d3ff48f5c0448037
John Marshall <marshall@hep.phy.cam.ac.uk>
2011-04-14 14:01:44 +0000

    Add command line switches and ability to turn event timing on/off.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/TestPandora/trunk@1133 e861a63a-b6b5-47dc-a5de-b4304083765c

--- src/PandoraInterface.cc
+++ src/PandoraInterface.cc
@@ -13,54 +13,97 @@
 
 #include "SimpleBFieldCalculator.h"
 
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <sys/time.h>
 
-static pandora::Pandora *m_pPandora;
+/**
+ *  @brief  Parameters class
+ */
+class Parameters
+{
+public:
+    /**
+     *  @brief Default constructor
+     */
+    Parameters();
 
-int main(int argc, const char *argv[])
+    std::string     m_pandoraSettingsFile;          ///< The path to the pandora settings file (mandatory parameter)
+    int             m_nEventsToProcess;             ///< The number of events to process (default all events in file)
+    bool            m_shouldDisplayEventTime;       ///< Whether event times should be calculated and displayed (default false)
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+ *  @brief  Parse the command line arguments, setting the application parameters
+ * 
+ *  @param  argc argument count
+ *  @param  argv argument vector
+ *  @param  parameters to receive the application parameters
+ * 
+ *  @return success
+ */
+bool ParseCommandLine(int argc, char *argv[], Parameters &parameters);
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
 {
     try
     {
-        // Parse command line
-        const int nArgs(argc - 1);
+        // Parse command line parameters
+        Parameters parameters;
 
-        if ((nArgs < 1) || (nArgs > 2))
-        {
-            std::cout << std::endl << "Usage: ./PandoraInterface PandoraSettings.xml [nEventsToProcess]" << std::endl << std::endl;
+        if (!ParseCommandLine(argc, argv, parameters))
             return 1;
-        }
 
         // Construct pandora instance
-        m_pPandora = new pandora::Pandora();
+        pandora::Pandora *pPandora = new pandora::Pandora();
 
         // Register content from external pandora libraries
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetPseudoLayerCalculator(*m_pPandora,
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetPseudoLayerCalculator(*pPandora,
             new FineGranularityPseudoLayerCalculator()));
 
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetShowerProfileCalculator(*m_pPandora,
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetShowerProfileCalculator(*pPandora,
             new FineGranularityShowerProfileCalculator()));
 
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, FineGranularityContent::RegisterAlgorithms(*m_pPandora));
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, FineGranularityContent::RegisterHelperFunctions(*m_pPandora));
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, KMeansContent::RegisterAlgorithms(*m_pPandora));
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, FineGranularityContent::RegisterAlgorithms(*pPandora));
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, FineGranularityContent::RegisterHelperFunctions(*pPandora));
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, KMeansContent::RegisterAlgorithms(*pPandora));
 
         // Register local content
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetBFieldCalculator(*m_pPandora,
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::SetBFieldCalculator(*pPandora,
             new SimpleBFieldCalculator()));
 
         // Read in the PandoraSettings
-        const std::string settingsFile(argv[1]);
-        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ReadSettings(*m_pPandora, settingsFile));
+        PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ReadSettings(*pPandora, parameters.m_pandoraSettingsFile));
 
         // Process the events
-        const int nEventsToProcess((nArgs == 2) ? atoi(argv[2]) : -1);
         int nEvents(0);
 
-        while ((nEvents++ < nEventsToProcess) || (0 > nEventsToProcess))
+        while ((nEvents++ < parameters.m_nEventsToProcess) || (0 > parameters.m_nEventsToProcess))
         {
-            PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ProcessEvent(*m_pPandora));
-            PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::Reset(*m_pPandora));
+            struct timeval startTime, endTime;
+
+            if (parameters.m_shouldDisplayEventTime)
+            {
+                (void) gettimeofday(&startTime, NULL);
+            }
+
+            PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::ProcessEvent(*pPandora));
+
+            if (parameters.m_shouldDisplayEventTime)
+            {
+                (void) gettimeofday(&endTime, NULL);
+                std::cout << "Event time " << (endTime.tv_sec + (endTime.tv_usec / 1.e6) - startTime.tv_sec - (startTime.tv_usec / 1.e6)) << std::endl;
+            }
+
+            PANDORA_THROW_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::Reset(*pPandora));
         }
+
+        delete pPandora;
     }
     catch (pandora::StatusCodeException &statusCodeException)
     {
@@ -69,4 +112,45 @@ int main(int argc, const char *argv[])
     }
 
     return 0;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool ParseCommandLine(int argc, char *argv[], Parameters &parameters)
+{
+    int c(0);
+
+    while ((c = getopt(argc, argv, "i:n:t::")) != -1)
+    {
+        switch (c)
+        {
+        case 'i':
+            parameters.m_pandoraSettingsFile = optarg;
+            break;
+        case 'n':
+            parameters.m_nEventsToProcess = atoi(optarg);
+            break;
+        case 't':
+            parameters.m_shouldDisplayEventTime = true;
+            break;
+        case '?':
+        default:
+            std::cout << std::endl << "./bin/PandoraInterface " << std::endl
+                      << "    -i PandoraSettings.xml  (mandatory)" << std::endl
+                      << "    -n NEventsToProcess     (optional)" << std::endl
+                      << "    -t                      (optional, display event times)" << std::endl << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+Parameters::Parameters() :
+    m_nEventsToProcess(-1),
+    m_shouldDisplayEventTime(false)
+{
 }
